b7a0be87f990bb480cb250a5ad7747c29a53a6ba
Alexander Nosevich <alexandernosevich@icloud.com>
2026-02-16 22:54:22 +0300

    Переписал задачу с использованием Deque

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-
-//Задание 1: StringBuilder и undo()
-
 public class Main {
     public static void main(String[] args) {
         PrettierStringBuilder sb = new PrettierStringBuilder();
-        //String name = stringBuilder.append("Alexander").append(" ").append("Nosevich").toString();
-        sb.append("Alexander");
-        //System.out.println(sb.toString());
-        System.out.println(sb);
 
-        sb.append(" ");
-        sb.append("Nosevich");
-        System.out.println(sb);
-
-        sb.append("bla-bla-bla");
-        System.out.println(sb);
-
+        sb.append("first world");
+        sb.append("second world");
         sb.undo();
-        System.out.println(sb);
-
+        String result = sb.toString();
+        System.out.println(result);
     }
 }
